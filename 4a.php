1e3056e556fa81ab6ebd32ba750b4c388927e0eb
SabikunNaharTanha <sabikunnahartanha6@gmail.com>
2022-01-10 11:57:19 +0600

    hospital websiite-commited

---
+++ 4a.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Printing a,b,c,d in a alert box</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<script type="text/javascript">
+		const a=prompt("enter a");
+        const b=prompt("enter b");
+        const c=prompt("enter c");
+        const d=prompt("enter d");
+
+        if(a>=b && c<=d)
+        {
+        	alert("the result is true");
+        }
+        else
+        {
+        	alert("the result is false");
+        }
+	</script>
+
+</body>
+</html>
---
+++ Start.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php starting</title>
+</head>
+<body>
+	<ul>
+	<<?php 
+      
+    echo "this is my first program";
+
+	 ?>
+     </ul>
+</body>
+</html>
